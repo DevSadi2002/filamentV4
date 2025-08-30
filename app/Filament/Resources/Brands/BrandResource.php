b9d33b70519bc9d300d4aaeb17942364248fec68
Saadi Dev <devsadi2002@gmail.com>
2025-08-30 17:19:53 +0300

    category and brand resource completed

--- app/Filament/Resources/Brands/BrandResource.php
+++ app/Filament/Resources/Brands/BrandResource.php
@@ -43,7 +43,7 @@ class BrandResource extends Resource
     {
         return [
             'index' => ListBrands::route('/'),
-            'create' => CreateBrand::route('/create'),
+            // 'create' => CreateBrand::route('/create'),
             'edit' => EditBrand::route('/{record}/edit'),
         ];
     }
