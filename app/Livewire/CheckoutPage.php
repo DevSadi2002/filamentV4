8cd5ee6263daa1ce003e1b1e88611c8aa62e8051
Saadi Dev <devsadi2002@gmail.com>
2025-09-07 15:47:36 +0300

    new version checkout ep-21

--- app/Livewire/CheckoutPage.php
+++ app/Livewire/CheckoutPage.php
@@ -2,12 +2,35 @@
 
 namespace App\Livewire;
 
+use App\Helpers\CartMangement;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
+#[Title('Checkout')]
 class CheckoutPage extends Component
 {
+    public $first_name;
+    public $last_name;
+    public $phone;
+    public $street_address;
+    public $city;
+    public $state;
+    public $zip_code;
+    public $payment_method;
+
+    public function placeOrder() {
+        $this->validate([
+            'first_name'
+        ]);
+    }
+
     public function render()
     {
-        return view('livewire.checkout-page');
+        $cart_items = CartMangement::getCartItemsFromCookie();
+        $grand_total = CartMangement::calculateGrandTotal($cart_items);
+        return view('livewire.checkout-page', [
+            'cart_items' => $cart_items,
+            'grand_total' => $grand_total
+        ]);
     }
 }
